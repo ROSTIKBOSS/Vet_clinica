c6efec165e8b7d0d401b496f82ad67c3ae3aa218
thegrep <den.belotserkovets@gmail.com>
2018-11-11 22:44:59 +0200

    added config.properties

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,27 @@
 package com.company;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class Main {
 
-    private static String username = "root";
-    private static String password = "root";
-    private static String url = "jdbc:mysql://localhost:3306/cinema"+
-            "?verifyServerCertificate=false"+
-            "&useSSL=false"+
-            "&requireSSL=false"+
-            "&useLegacyDatetimeCode=false"+
-            "&amp"+
-            "&serverTimezone=UTC";
+    private static String username;
+    private static String password;
+    private static String url;
+
+
+    private static void _init_() throws IOException {
+        FileInputStream fis;
+        Properties properties = new Properties();
+        fis = new FileInputStream("src/config.properties");
+        properties.load(fis);
+        username = properties.getProperty("db.login");
+        url = properties.getProperty("db.host");
+        password = properties.getProperty("db.password");
+    }
+
     public static void main(String[] args) throws IOException, SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -21,6 +29,7 @@ public class Main {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        _init_();
         Connection connection = DriverManager.getConnection(url,username,password);
 
         String query = "select sername, name, num from client order by sername asc;\n";
@@ -33,7 +42,6 @@ public class Main {
             String name = res.getString("name");
             int num = res.getInt("num");
             System.out.format("%s, %s, %s\n", sername,name,num);
-
         }
     }
 }
